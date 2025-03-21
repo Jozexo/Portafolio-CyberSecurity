3f6e4fc1d3c6354e7cb02ab0a797d313e0de08ca
Jozexo <josegollitohernandezr@gmail.com>
2025-03-21 19:27:06 -0400

    connect bd

---
+++ php/enviar_comentario.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "portafolio";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$usuario = $_POST['usuario'];
+$email = $_POST['email'];
+$nota = $_POST['nota'];
+$fechanota = date("Y-m-d H:i:s");
