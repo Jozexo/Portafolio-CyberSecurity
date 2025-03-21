96b8a61fbe33369508362dccbb29ecd687b44b7c
Jozexo <josegollitohernandezr@gmail.com>
2025-03-21 19:29:23 -0400

    consulta comment

--- php/cargar_comentario.php
+++ php/cargar_comentario.php
@@ -5,3 +5,10 @@ $password = "";
 $dbname = "portafolio";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT nombre, apellido, usuario, nota, fechanota FROM comentarios ORDER BY fechanota DESC";
+$result = $conn->query($sql);
