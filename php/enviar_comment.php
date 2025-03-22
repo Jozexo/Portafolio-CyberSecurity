21c8efe3aa62a2465d1d5e9cd68ba4f59b4e8f2a
Jozexo <josegollitohernandezr@gmail.com>
2025-03-21 23:50:28 -0400

    update enviar conect bd

--- php/enviar_comment.php
+++ php/enviar_comment.php
@@ -1,7 +1,19 @@
 <?php
-include 'config.php';
+//include 'config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cybersecurity_comments";
+$conn = "";
+
+$conn = mysqli_connect(
+    $servername,
+    $username,
+    $password,
+    $dbname
+);
+
     $nombreyapellido = $_POST['nombreyapellido'];
     $usuario = $_POST['usuario'];
     $email = $_POST['email'];
@@ -18,5 +30,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $conn->close();
-}
-?>
+//}
